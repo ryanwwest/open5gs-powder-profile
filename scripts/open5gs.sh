01d220d788e76f5bf7d159127cd69ad3eac312fb
ryanwest6 <ryanwest6@gmail.com>
2021-03-31 10:40:23 -0600

    add needed configs for ran and make fixes
    Signed-off-by: ryanwest6 <ryanwest6@gmail.com>

--- scripts/open5gs.sh
+++ scripts/open5gs.sh
@@ -1,13 +1,19 @@
+# print every command
+set -x
+
+
 # install open5gs
 apt -y update
 apt -y install software-properties-common
 add-apt-repository ppa:open5gs/latest
 apt -y update
-apt -y install open5gs # might have to press enter here
+apt -y install npm open5gs # might have to press enter here
+
+echo Now install Open5GS Web UI
 
 # install open5gs web UI
-sudo apt -y install curl nodejs
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
+apt -y install nodejs
 curl -sL https://open5gs.org/open5gs/assets/webui/install | bash -
 #curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 #curl -sL https://open5gs.org/open5gs/assets/webui/install | sudo -E bash -
