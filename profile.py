42f36e13cb57f25ebebd799827fe985477951ec7
ryanwest6 <ryan@ryanwwest.com>
2021-05-06 13:01:14 -0600

    fix ue param bug
    Signed-off-by: ryanwest6 <ryan@ryanwwest.com>

--- profile.py
+++ profile.py
@@ -45,7 +45,7 @@ class GLOBALS(object):
 
 def invoke_script_str(filename):
     # populate script config before running scripts (replace '?'s)
-    populate_config = f"sed -i "" 's/NUM_UE_=?/NUM_UE_={params.uenum}' " + SCRIPT_DIR + SCRIPT_CONFIG
+    populate_config = f"sed -i '' 's/NUM_UE_=?/NUM_UE_={str(params.uenum)}' " + SCRIPT_DIR + SCRIPT_CONFIG
     # also redirect all output to /script_output
     run_script = "sudo bash " + GLOBALS.SCRIPT_DIR + filename + " &> ~/install_script_output"
     return populate_config + " && " + run_script
